fd393348fa08f390b3f0006010d1782e251925a9
Damir <damir8520@gmail.com>
2020-04-04 17:41:40 +0100

    created dashboard component. routed the dashboard component as a child of the the default component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DefaultComponent } from './layouts/default/default.component';
+import { DefaultModule } from './layouts/default/default.module';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DefaultComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DefaultModule
     
   ],
   providers: [],
