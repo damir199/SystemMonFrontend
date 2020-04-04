ecc29fe6be9968ec58e6415931d9251c3aeb2086
Damir <damir8520@gmail.com>
2020-04-04 20:04:05 +0100

    completing menu items in sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,8 +9,11 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router'
+
 
 @NgModule({
   declarations: [
@@ -26,6 +29,8 @@ import { MatMenuModule } from '@angular/material/menu';
     MatButtonModule,
     FlexLayoutModule,
     MatMenuModule,
+    MatListModule,
+    RouterModule,
 
   
   ],
