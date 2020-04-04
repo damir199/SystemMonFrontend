338e75f80540a86b3b2f2a7f99fef427bee92bb6
Damir <damir8520@gmail.com>
2020-04-04 19:08:26 +0100

    added basic menu item to sign out of dashboard. non functioning currently.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,7 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -25,6 +25,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatIconModule,
     MatButtonModule,
     FlexLayoutModule,
+    MatMenuModule,
 
   
   ],
